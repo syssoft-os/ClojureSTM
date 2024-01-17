79a5a7f0e9c6573e666dc71f7c1a46591e211e1a
Peter Sturm <sturm@uni-trier.de>
2024-01-17 20:43:32 +0100

    Thread pool stuff not working
    CoPilot was halucinating?

--- src/clj_stm/core.clj
+++ src/clj_stm/core.clj
@@ -13,25 +13,20 @@
    ; (Thread/sleep 10)
    (alter y + amount)))
 
-(import '[java.util.concurrent Executors])
-
 (defn -main
   [& args]
   ; The argument is the number of transfers to perform
   (let [num-transfers (Integer/parseInt (first args))
-        executor (Executors/newFixedThreadPool 1)  ; Create a thread pool
         ; Create a list of futures, each of which transfers $1 from A to B and back again
         futures (for [n (range num-transfers)]
-                  [(future (transfer account-a account-b 1) executor)
-                   (future (transfer account-b account-a 1) executor)])]
+                  [(future (transfer account-a account-b 1))
+                   (future (transfer account-b account-a 1))])]
     ; Wait for all the futures to complete
     (doseq [f (flatten futures)] (deref f))
     ; Print the balances, the accounts should be back where they started
     (println "Account A: " @account-a)
     (println "Account B: " @account-b)
     ; Print the counter
-    (println "Counter: " @counter)
-    ; Shut down the thread pool
-    (.shutdown executor))
+    (println "Counter: " @counter))
   ; Shut down the agents that are used by the STM
   (shutdown-agents))
