faab7f54cc1cc3b1d4cc8100e00c52ef51974ac2
Peter Sturm <sturm@uni-trier.de>
2024-01-17 20:18:07 +0100

    executor added
    Still the number of dosync’s doesn’t show any retries.

--- src/clj_stm/core.clj
+++ src/clj_stm/core.clj
@@ -9,14 +9,17 @@
 (defn transfer [x y amount]
   (dosync
    (alter x - amount)
-   (Thread/sleep 100)
+   (Thread/sleep 10)
    (alter y + amount)
    (swap! counter inc)))
+
+(import '[java.util.concurrent Executors])
 
 (defn -main
   [& args]
   ; The argument is the number of transfers to perform
   (let [num-transfers (Integer/parseInt (first args))
+        executor (Executors/newFixedThreadPool 10)  ; Create a thread pool with 10 threads
         ; Create a list of futures, each of which transfers $1 from A to B and back again
         futures (for [n (range num-transfers)]
                   [(future (transfer account-a account-b 1))
@@ -27,6 +30,8 @@
     (println "Account A: " @account-a)
     (println "Account B: " @account-b)
     ; Print the counter
-    (println "Counter: " @counter))
+    (println "Counter: " @counter)
+    ; Shut down the thread pool
+    (.shutdown executor))
   ; Shut down the agents that are used by the STM
   (shutdown-agents))
